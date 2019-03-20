88758291ec5e2cdd11e44cbfe29cd3a114a709de
DX_Hwkang <373496706@qq.com>
2019-03-20 17:17:32 +0800

    add python

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import pygame
+from setting import Setting
+from ship import Ship
+import game_function as gf
+
+def run_game():
+    #初始化游戏，创建屏幕对象
+    pygame.init()
+    ai_settings = Setting()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_heigth))
+    pygame.display.set_caption("alien_invasion") 
+
+    #创建一艘飞船
+    ship = Ship(screen)
+
+    #开始游戏主循环
+    while True:
+
+        #监视键鼠
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+
+
+run_game()
---
+++ alien_invasion/setting.py
@@ -0,0 +1,12 @@
+class Setting():
+    """储存外星人入侵所有的类"""
+
+    def __init__(self):
+        """初始化游戏设置"""
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_heigth = 600
+        self.bg_color = (230,230,230)
+        
+        #飞船设置
+        self.ship_
