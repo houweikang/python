404cff2dd419f05de47f85e0d52818035f78f885
DX_Hwkang <373496706@qq.com>
2019-03-21 17:30:41 +0800

    del git stage

--- mpl_squares.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x_range = list(range(1,1001))
-y = [x**3 for x in x_range]
-plt.scatter(x_range,y,c=y,cmap=plt.cm.Blues,edgecolors='none',s=40)
-#plt.axis([0,1100,0,1100000])
-
-
-plt.show()
